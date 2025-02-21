4cd3d0a220e56100cdfe5e3cc1165a03e3a4a56f
Brenda Tapari <99502@sistemas.frc.utn.edu.ar>
2025-02-21 19:39:38 -0300

    aplicando imagen de fondo

--- src/renderer/src/components/login.jsx
+++ src/renderer/src/components/login.jsx
@@ -4,47 +4,55 @@ import { useLocation } from 'wouter'
 export default function Login() {
   const [, setLocation] = useLocation()
   return (
-    <div
-      className="flex items-center justify-center min-h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('./src/images/background.jpg')" }}
-    >
-      <div className="card glass flex-row w-[32rem] bg-gray-800 bg-opacity-50 text-white shadow-xl p-6 rounded-lg">
-        <Settings />
-        {/* Imagen alineada a la izquierda */}
-        <figure className="flex items-center justify-center px-4">
-          <div className="avatar">
-            <div className="w-32 h-32 rounded-full border-4 border-gray-500 flex items-center justify-center">
-              <img src="./src/images/user_icon.webp" alt="User Icon" />
+    <div className=" image-full w-full z-0 bg-cover bg-center relative">
+      <figure className="absolute inset-0 ">
+        <img
+          src="./src/images/sunset2.jpg"
+          alt="bgImage"
+          className="w-full h-full  rounded-none"
+        />
+      </figure>
+      
+      <div className="relative z-20 flex items-center justify-center min-h-screen">
+        <div className="card glass flex-row w-[32rem] bg-gray-800 bFg-opacity-50 text-white shadow-xl p-2 rounded-lg">
+          <Settings />
+          <figure className="flex items-center justify-center px-4">
+            <div className="avatar">
+              <div className="w-32 h-32 rounded-full border-4 border-gray-500 flex items-center justify-center">
+                <img src="./src/images/user_icon.webp" alt="User Icon" />
+              </div>
+            </div>
+          </figure>
+
+          <div className="card-body flex-1">
+            <h2 className="text-center text-2xl font-semibold mb-2">Iniciar Sesión</h2>
+            <input
+              type="text"
+              className="input input-bordered w-full mb-2 text-black"
+              required
+              placeholder="Usuario"
+              pattern="[A-Za-z][A-Za-z0-9\-]*"
+              minLength="3"
+              maxLength="30"
+            />
+            <input
+              type="password"
+              className="input input-bordered w-full mb-4 text-black"
+              required
+              placeholder="Contraseña"
+              minLength="8"
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            />
+            <div className="card-actions flex justify-end">
+              <button className="btn btn-primary" onClick={() => setLocation('/home')}>
+                Iniciar
+              </button>
             </div>
           </div>
-        </figure>
-
-        {/* Cuerpo del formulario */}
-        <div className="card-body flex-1">
-          <h2 className="text-center text-2xl font-semibold mb-4">Iniciar Sesión</h2>
-          <input
-            type="text"
-            className="input input-bordered w-full mb-2 text-black"
-            required
-            placeholder="Username"
-            pattern="[A-Za-z][A-Za-z0-9\-]*"
-            minLength="3"
-            maxLength="30"
-          />
-          <input
-            type="password"
-            className="input input-bordered w-full mb-4 text-black"
-            required
-            placeholder="Password"
-            minLength="8"
-            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-          />
-          <div className="card-actions flex justify-end">
-            <button className="btn btn-primary" onClick={() => setLocation('/home')}>
-              Iniciar
-            </button>
-          </div>
         </div>
+      </div>
+      <div className="absolute bottom-0 left-0 p-4">
+        <p className="font-bold text-6xl text-white "> Mykonos-OS</p>
       </div>
     </div>
   )
