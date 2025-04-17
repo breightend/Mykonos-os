8123ea569516cb8ca3588a2aacfca83b68670311
G0nzalito <zavalagonzalo172@gmail.com>
2025-04-16 21:14:05 -0300

    volviendo todo a la normalidad

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,4 +1,3 @@
-import atexit
 import webbrowser
 import threading
 from flask import Flask, jsonify, request
@@ -9,7 +8,6 @@ from werkzeug.security import generate_password_hash
 from routes.usuario_router import usuario_router
 from routes.provider_router import provider_router
 from routes.client_router import client_router
-from codecarbon import EmissionsTracker
 
 app = Flask(__name__)
 app.register_blueprint(usuario_router, url_prefix="/api/data")
@@ -22,7 +20,7 @@ CORS(
             "allow_headers": ["Content-Type"],
         }
     },
-)
+)  # Habilitar CORS para todas las rutas
 app.register_blueprint(provider_router, url_prefix="/api/provider")
 app.register_blueprint(client_router, url_prefix="/api/client")
 
@@ -42,26 +40,15 @@ def open_browser():
     webbrowser.open_new("http://127.0.0.1:5000/")
 
 
-# @atexit.register
-# def cleanup():
-#     print("Ejecutando limpieza antes de salir...")
-#     print("Cerrando la base de datos...")
-#     i = 100000
-#     db = Database()
-#     while i > 0:
-#         i -= 1
-#         db.get_record_by_id("entities", 1)
-#         print(i)
-#     if "tracker" in globals():
-#         tracker.stop()
+""" @app.route('/api/data', methods=['POST'])
+def getData():
+    data=request.json
+    print(data)
+    return jsonify({"mensaje": "Hola desde Flask", "status": "éxito"}) """
 
 
 if __name__ == "__main__":
     # Inicia un temporizador para abrir el navegador después de 1 segundo
     # threading.Timer(1, open_browser).start()
-
-    # tracker = EmissionsTracker(default_cpu_power=85, log_level="debug")
-    # tracker.start()
-
-    # Ejecutar Flask sin recargador para evitar problemas con señales
-    app.run(debug=True, use_reloader=False, port=5000)
+    app.run(debug=True, port=5000)
+    # create_admin.create_admin()
