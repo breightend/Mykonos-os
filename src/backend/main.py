9cf2a23bf1a7958c6e59bfef9faf7259d0c9ccdc
G0nzalito <zavalagonzalo172@gmail.com>
2025-04-16 21:07:43 -0300

    agregando el codecarbon

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,3 +1,4 @@
+import atexit
 import webbrowser
 import threading
 from flask import Flask, jsonify, request
@@ -8,40 +9,59 @@ from werkzeug.security import generate_password_hash
 from routes.usuario_router import usuario_router
 from routes.provider_router import provider_router
 from routes.client_router import client_router
+from codecarbon import EmissionsTracker
 
 app = Flask(__name__)
-app.register_blueprint(usuario_router, url_prefix='/api/data')
-CORS(app, resources={
-    r"/api/*": {
-        "origins": "http://localhost:5173",
-        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        "allow_headers": ["Content-Type"]
-    }
-})  # Habilitar CORS para todas las rutas
-app.register_blueprint(provider_router, url_prefix='/api/provider')
-app.register_blueprint(client_router, url_prefix='/api/client')
-@app.route('/')
+app.register_blueprint(usuario_router, url_prefix="/api/data")
+CORS(
+    app,
+    resources={
+        r"/api/*": {
+            "origins": "http://localhost:5173",
+            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            "allow_headers": ["Content-Type"],
+        }
+    },
+)
+app.register_blueprint(provider_router, url_prefix="/api/provider")
+app.register_blueprint(client_router, url_prefix="/api/client")
+
+
+@app.route("/")
 def index():
     create_admin.create_admin()
     return "¡Hola, mundo desde Flask!"
 
-@app.route('/saludo')
+
+@app.route("/saludo")
 def get_data():
     return jsonify({"mensaje": "Hola desde Flask", "status": "éxito"})
+
 
 def open_browser():
     webbrowser.open_new("http://127.0.0.1:5000/")
 
-""" @app.route('/api/data', methods=['POST'])
-def getData():
-    data=request.json
-    print(data)
-    return jsonify({"mensaje": "Hola desde Flask", "status": "éxito"}) """
+
+# @atexit.register
+# def cleanup():
+#     print("Ejecutando limpieza antes de salir...")
+#     print("Cerrando la base de datos...")
+#     i = 100000
+#     db = Database()
+#     while i > 0:
+#         i -= 1
+#         db.get_record_by_id("entities", 1)
+#         print(i)
+#     if "tracker" in globals():
+#         tracker.stop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Inicia un temporizador para abrir el navegador después de 1 segundo
-    #threading.Timer(1, open_browser).start()
-    app.run(debug=True, port=5000)
-    #create_admin.create_admin()
-    
+    # threading.Timer(1, open_browser).start()
+
+    # tracker = EmissionsTracker(default_cpu_power=85, log_level="debug")
+    # tracker.start()
+
+    # Ejecutar Flask sin recargador para evitar problemas con señales
+    app.run(debug=True, use_reloader=False, port=5000)
