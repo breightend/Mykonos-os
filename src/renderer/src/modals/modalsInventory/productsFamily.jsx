5f36557afd3498b92d454aed56ec33391cb94f67
Brenda Tapari <brendatapa6@gmail.com>
2025-04-26 19:44:27 -0300

    Productos arreglados

--- src/renderer/src/modals/modalsInventory/productsFamily.jsx
+++ src/renderer/src/modals/modalsInventory/productsFamily.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { postFamilyData, fetchFamilyProducts } from '../../services/products/familyService'
+import { Plus, Minus } from 'lucide-react'
 
 export default function ProductsFamily() {
   const [formData, setFormData] = useState({
@@ -7,39 +8,103 @@ export default function ProductsFamily() {
     parent_group_id: '',
     marked_as_root: ''
   })
+  const [familyGroup, setFamilyGroup] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetchFamilyProducts()
-        if (response) {
-          setFormData({
-            group_name: response.group_name || '',
-            parent_group_id: response.parent_group_id || '',
-            marked_as_root: response.marked_as_root || ''
-          })
-        }
+        const familyGroupResponse = await fetchFamilyProducts()
+        setFamilyGroup(familyGroupResponse)
+        console.log('Familia de productos:', familyGroupResponse)
       } catch (error) {
         console.error('Error fetching family products:', error)
       }
     }
     fetchData()
   }, [])
+  const [mostrarAgregarFamilia, setMostrarAgregarFamilia] = useState(false)
+
+  const handleAgregarALaFamilia = () => {
+    setMostrarAgregarFamilia(!mostrarAgregarFamilia)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await postFamilyData(formData)
+      console.log('Familia de productos guardada:', response)
+      setFormData({
+        group_name: '',
+        parent_group_id: '',
+        marked_as_root: ''
+      })
+    } catch (error) {
+      console.error('Error saving family product:', error)
+    }
+  }
+
   return (
     <div>
       <dialog id="productsFamily" className="modal">
         <div className="modal-box max-w-5xl">
           <h3 className="text-lg font-bold">Familia de productos</h3>
-          <div className="flex flex-col gap-4">
-            <label htmlFor="" className="">
-              Nombre de la familia:{' '}
-            </label>
-            <input type="text" className="input" placeholder="Nombre de la familia" />
-            <label htmlFor="">Descripción: </label>
-            <input type="text" className="input" placeholder="Descripción" />
-            <label htmlFor="">Observaciones: </label>
-            <input type="text" className="input" placeholder="Observaciones" />
-          </div>
+
+          {!mostrarAgregarFamilia ? (
+            <>
+              <button
+                className="btn btn-icon btn-ghost btn-circle rotate-90 transition-all transition-discrete duration-350"
+                onClick={handleAgregarALaFamilia}
+              >
+                <Plus />
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                className="btn btn-icon btn-ghost btn-circle tooltip transition-all transition-discrete duration-350"
+                onClick={handleAgregarALaFamilia}
+                data-tip="Mostrar menos"
+              >
+                <Minus />
+              </button>
+              <div className="flex flex-col gap-4">
+                {familyGroup && (
+                  <>
+                    <label htmlFor="" className="">
+                      Selecciona la familia de productos:{' '}
+                    </label>
+                    <select
+                      className="select select-bordered w-full max-w-xs"
+                      name="parent_group_id"
+                      onChange={(e) => {
+                        setFormData({ ...formData, parent_group_id: e.target.value })
+                      }}
+                    >
+                      <option value="">Selecciona una familia</option>
+                      {familyGroup.map((group) => (
+                        <option key={group.id} value={group.id}>
+                          {group.group_name}
+                        </option>
+                      ))}
+                    </select>
+                  </>
+                )}
+                <label htmlFor="" className="">
+                  Nombre de la familia:{' '}
+                </label>
+                <input
+                  type="text"
+                  className="input"
+                  name="group_name"
+                  placeholder="Nombre de la familia"
+                />
+              </div>
+              <btn className="btn btn-primary" onClick={handleSubmit}>
+                + Agregar
+              </btn>
+            </>
+          )}
+
           <form method="dialog">
             <div className="modal-action">
               <button className="btn btn-success">Guardar</button>
